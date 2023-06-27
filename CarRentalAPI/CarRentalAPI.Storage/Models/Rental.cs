0b710b7b70c30d7d8c480dcc51d9eb3c35ebbd23
Mattu <mateusz.pl671@gmail.com>
2023-06-27 23:04:38 +0200

    add validation

--- CarRentalAPI/CarRentalAPI.Storage/Models/Rental.cs
+++ CarRentalAPI/CarRentalAPI.Storage/Models/Rental.cs
@@ -1,13 +1,27 @@
-﻿namespace CarRentalAPI.Storage.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalAPI.Storage.Models;
 
 public class Rental
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required]
     public int CarId { get; set; }
     public int? UserId { get; set; }
+    [Required]
+    [MaxLength(50)]
     public string CustomerFirstName { get; set; }
+    [Required]
+    [MaxLength(50)]
     public string CustomerLastName { get; set; }
+    [Required]
+    [EmailAddress]
     public string CustomerEmail { get; set; }
+    [Required]
     public DateTime StartDate { get; set; }
+    [Required]
     public DateTime EndDate { get; set; }
 }
